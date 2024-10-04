ec717701c551fb94e87acc1ae8b08b60399f3b08
Santiago Fleitas <sansfleitas@gmail.com>
2024-10-03 22:39:26 -0300

    Finish client unit tests

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -39,3 +39,14 @@ func (c *Client) Validate() error {
 	}
 	return nil
 }
+
+func (c *Client) Update(name string, email string) error {
+	c.Name = name
+	c.Email = email
+	c.UpdatedAt = time.Now()
+	err := c.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -19,3 +19,17 @@ func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Nil(t, client)
 }
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("John Doe Update", "j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Update", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "name is required")
+}
